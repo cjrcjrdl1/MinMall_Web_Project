bfbe56b3f14c54840ea2d124e4b8030d75656fad
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-14 10:10:25 +0900

    목록창 드디어 디버깅 성공

--- src/main/java/min/community/web/IndexController.java
+++ src/main/java/min/community/web/IndexController.java
@@ -1,20 +1,39 @@
 package min.community.web;
 
 import lombok.RequiredArgsConstructor;
+import min.community.domain.posts.Posts;
+import min.community.domain.posts.PostsRepository;
+import min.community.service.PostsService;
+import min.community.web.dto.PostsSaveRequestDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+    private final PostsRepository postsRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Posts> posts = postsRepository.findAll();
+        model.addAttribute("posts", posts);
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @PutMapping("/posts/save")
+    public String postsSaved() {
+        return "index";
     }
 }
