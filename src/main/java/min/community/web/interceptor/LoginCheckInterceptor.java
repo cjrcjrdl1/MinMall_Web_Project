72589e6207ca1ebd46ebc05182fa86277b471ab2
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-15 10:38:30 +0900

    인터셉터 구현 (로그인 검증, 로그)

---
+++ src/main/java/min/community/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,28 @@
+package min.community.web.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import min.community.web.SessionConst;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+
+        log.info("인증 여부 확인 [{}]", requestURI);
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+            log.info("미인증 사용자 요청");
+
+            response.sendRedirect("/login?redirectURL=" + requestURI);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/min/community/web/interceptor/WebConfig.java
@@ -0,0 +1,25 @@
+package min.community.web.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/js/**");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error", "/js/**");
+    }
+
+
+}
