80f364d353e5b4801659e5924ba718db2120ee94
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-05-19 19:56:11 +0900

    .

--- src/main/java/min/community/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/min/community/web/interceptor/LoginCheckInterceptor.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
+
+
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
